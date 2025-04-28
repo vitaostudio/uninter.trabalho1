c1a94e7a9fd06bc247235e8548d334a4686cdf6d
vitaostudio <victor.studio@outlook.com>
2025-04-28 18:16:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("#botao1");
+let textoEscondido = document.querySelector("#escondido");
+let aparecer = false;
+
+botao.addEventListener("click", e => {
+    textoEscondido.style.background="white";
+})
+
